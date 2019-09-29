0b31922d863066ca4efccdd295fcef97c4b80cdb
khalid-m-salama <khalid.m.salama@gmail.com>
2019-09-29 10:14:30 +0100

    Updates to text2emb

---
+++ text2emb/cloud_trainer/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='text-cooc2emb',
+    version='v1',
+    packages=find_packages(),
+    include_package_data=True
+)
