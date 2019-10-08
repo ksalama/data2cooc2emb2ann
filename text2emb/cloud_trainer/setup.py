446028c4375b2c27b37b4ac43605b8ffd7d9e126
khalid-m-salama <khalid.m.salama@gmail.com>
2019-10-08 15:41:38 +0100

    Remove cloud trainer

--- text2emb/cloud_trainer/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='text-cooc2emb',
-    version='v1',
-    packages=find_packages(),
-    include_package_data=True
-)
